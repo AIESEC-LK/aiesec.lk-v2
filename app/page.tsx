edc8499cec5852783b004ae16866c437e515ebe7
AmzalFoumi <147682418+AmzalFoumi@users.noreply.github.com>
2025-10-01 11:40:07 +0530

    feat: our story

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
+import OurStory from "@/components/OurStory";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div >
-
-
-     
+    <div>
+      <OurStory />
     </div>
   );
 }
