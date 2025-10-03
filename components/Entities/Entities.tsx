334f2c93cbbfdf71eb68863f3dd9d9fe72deef22
charindithjaindu <peter.9102feb@gmail.com>
2025-10-03 20:04:37 +0530

    feat: added liquidether effect into oppotunities and entities sections

--- components/Entities/Entities.tsx
+++ components/Entities/Entities.tsx
@@ -8,22 +8,26 @@ import {
   entitiesCtaLink,
 } from "../../constants/entities";
 import { EntitiesGrid } from "./subcomponents/EntitiesGrid";
+import SpotlightWrapper from "@/components/SpotlightWrapper";
 
 export function Entities() {
+  const spotlightColor = `rgba(3, 126, 243, 0.25)`;
   return (
-    <section id="entities" className=" bg-white ">
-      <div className="container mx-auto px-4 lg:px-8">
-        <div className="max-w-3xl mx-auto text-center mb-16">
-          <h2 className="text-4xl md:text-6xl font-bold mb-6 text-balance">
-            {entitiesHeader}
-          </h2>
-          <p className="text-xl text-muted-foreground leading-relaxed text-pretty">
-            {entitiesSubheader}
-          </p>
+    <SpotlightWrapper spotlightColor={spotlightColor}>
+      <section id="entities" className=" bg-white ">
+        <div className="container mx-auto px-4 lg:px-8">
+          <div className="max-w-3xl mx-auto text-center mb-16">
+            <h2 className="text-4xl md:text-6xl font-bold mb-6 text-balance">
+              {entitiesHeader}
+            </h2>
+            <p className="text-xl text-muted-foreground leading-relaxed text-pretty">
+              {entitiesSubheader}
+            </p>
+          </div>
+          <EntitiesGrid entities={entities} />
+
         </div>
-        <EntitiesGrid entities={entities} />
-        
-      </div>
-    </section>
+      </section>
+    </SpotlightWrapper>
   );
 }
